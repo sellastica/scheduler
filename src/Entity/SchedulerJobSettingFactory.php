66f34bea9a653fc1072212095d7f13bbd64e20a2
Tomas Krejci <tomas@differencemakers.cz>
2018-07-11 08:45:20 +0200

    .

--- src/Entity/SchedulerJobSettingFactory.php
+++ src/Entity/SchedulerJobSettingFactory.php
@@ -3,42 +3,13 @@ namespace Sellastica\Scheduler\Entity;
 
 use Sellastica\Entity\Entity\EntityFactory;
 use Sellastica\Entity\Entity\IEntity;
-use Sellastica\Entity\EntityManager;
-use Sellastica\Entity\Event\IDomainEventPublisher;
 use Sellastica\Entity\IBuilder;
-use Sellastica\Project\Entity\IProjectRepository;
-use Sellastica\Project\Model\ProjectAccessor;
 
 /**
  * @method SchedulerJobSetting build(IBuilder $builder, bool $initialize = true, int $assignedId = null)
  */
 class SchedulerJobSettingFactory extends EntityFactory
 {
-	/** @var \Sellastica\Project\Entity\IProjectRepository */
-	private $projectRepository;
-	/** @var \Sellastica\Project\Model\ProjectAccessor */
-	private $projectAccessor;
-
-
-	/**
-	 * @param \Sellastica\Entity\EntityManager $em
-	 * @param IDomainEventPublisher $eventPublisher
-	 * @param IProjectRepository $projectRepository
-	 * @param \Sellastica\Project\Model\ProjectAccessor $projectAccessor
-	 */
-	public function __construct(
-		EntityManager $em,
-		IDomainEventPublisher $eventPublisher,
-		IProjectRepository $projectRepository,
-		ProjectAccessor $projectAccessor
-	)
-	{
-		parent::__construct($em, $eventPublisher);
-
-		$this->projectRepository = $projectRepository;
-		$this->projectAccessor = $projectAccessor;
-	}
-
 	/**
 	 * @param IEntity|SchedulerJobSetting $entity
 	 */
